3e76d2cbed354ccb56c7d86a38d98e9c99bdc03d
Aman Bharti <amanbharti0112@gmail.com>
2026-08-14 17:10:16 +0530

    Add GitHub Actions CI pipeline

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js + GitHub Actions + Docker + Kubernetes!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "UP" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+});
